6ad2904d97eb681eb68ecea7c62b5bb88ee7b290
wildmouse <wildmouse0813@gmail.com>
2023-08-20 22:19:34 +0900

    Update versions

--- contracts/01_simple_nft/3_SimpleNFT.sol
+++ contracts/01_simple_nft/3_SimpleNFT.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.12;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract SimpleNFT is ERC721 {
-    
+
     constructor() ERC721("SimpleNFT", "MFT") {}
 
     function safeMint(uint256 tokenId) public {
