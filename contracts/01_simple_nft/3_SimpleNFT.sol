a3d0b82413002ae33c09eb1b93df7278face7ad8
wildmouse <wildmouse0813@gmail.com>
2023-08-20 22:20:30 +0900

    Update symbol

--- contracts/01_simple_nft/3_SimpleNFT.sol
+++ contracts/01_simple_nft/3_SimpleNFT.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract SimpleNFT is ERC721 {
 
-    constructor() ERC721("SimpleNFT", "MFT") {}
+    constructor() ERC721("SimpleNFT", "S") {}
 
     function safeMint(uint256 tokenId) public {
         _safeMint(msg.sender, tokenId);
